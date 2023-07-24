f0da52af9258d59bb91b2b611ec24e50f01d3cd8
Collins Karweru <collinskarweru@gmail.com>
2023-07-24 12:44:00 -0700

    commit

--- _print.c
+++ _print.c
@@ -1,22 +1,22 @@
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * _putchar - Helper function to write a single character to stdout.
+ *
+ * @c: The character to be written.
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1 and sets errno appropriately.
+ */
+static int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 
 /**
- * _printf - Custom printf function
- *
- * This function emulates the behavior of the standard printf function, allowing
- * formatted output to be printed to the standard output (stdout). It supports
- * the following format specifiers:
- *
- *   %c - Character: Prints a single character.
- *   %d - Decimal: Prints a signed integer in decimal format.
- *   %i - Integer: Same as %d, prints a signed integer in decimal format.
- *   %s - String: Prints a null-terminated string of characters.
- *   %R - ROT13 Encode: Prints a string encoded in ROT13 format.
- *   %r - Reverse String: Prints a string in reverse order.
+ * _printf - Custom printf function supporting %c, %s, and %% conversion specifiers.
  *
  * @format: A pointer to a string containing the format specifier and optional
  *          text to be printed.
@@ -25,70 +25,46 @@
  *
  * Return: The total number of characters successfully printed to stdout.
  *         If an error occurs, -1 is returned.
- *
- * Example:
- *     _printf("Hello, %s! My age is %d.\n", "Alice", 30);
- *
- * This function will print:
- *     "Hello, Alice! My age is 30."
  */
 int _printf(const char *format, ...)
 {
-	int idx, jdx, total_count, found;
-	va_list arg_list;
-	set print_functions[] = {
-		{'c', print_char},
-		{'d', print_d},
-		{'i', print_d},
-		{'s', print_str},
-		{'R', print_rot13},
-		{'r', print_rev},
-	};
+    int count = 0;
+    va_list args;
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return -1;
 
-	va_start(arg_list, format);
+    va_start(args, format);
 
-	total_count = 0;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; // Move past '%'
+            switch (*format)
+            {
+                case 'c':
+                    count += print_char(args);
+                    break;
+                case 's':
+                    count += print_str(args);
+                    break;
+                case '%':
+                    count += print_percent(args);
+                    break;
+                default:
+                    count += _putchar('%');
+                    count += _putchar(*format);
+            }
+        }
+        else
+        {
+            count += _putchar(*format);
+        }
+        format++;
+    }
 
-	for (idx = 0; *(format + idx); idx++)
-	{
-		if (*(format + idx) == '%')
-		{
-			if (*(format + idx + 1) == '\0')
-				continue;
-			found = 0;
-
-			for (jdx = 0; jdx < 6; jdx++)
-			{
-				if (*(format + idx + 1) == print_functions[jdx].spec)
-				{
-					total_count += print_functions[jdx].print(arg_list);
-					found = 1;
-					format++;
-					break;
-				}
-			}
-
-			if (found != 1)
-			{
-				if (*(format + idx + 1) == '%')
-				{
-					total_count += write(1, "%", 1);
-					format++;
-				}
-				else
-					total_count += write(1, (format + idx), 1);
-			}
-		}
-		else
-		{
-			total_count += write(1, (format + idx), 1);
-		}
-	}
-	va_end(arg_list);
-
-	return (total_count);
+    va_end(args);
+    return count;
 }
 
--- main.h
+++ main.h
@@ -1,28 +1,70 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * struct specifier - Struct to store format specifiers and their corresponding
- *                     printing functions.
+ * _putchar - Helper function to write a single character to stdout.
  *
- * @spec: The format specifier character.
- * @print: The function pointer to the corresponding printing function.
+ * @c: The character to be written.
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1 and sets errno appropriately.
  */
-typedef struct specifier
+static int _putchar(char c)
 {
-	char spec;
-	int (*print)(va_list);
-} set;
+    return write(1, &c, 1);
+}
 
-/* Function prototypes */
-int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_d(va_list args);
-int print_str(va_list args);
-int print_rot13(va_list args);
-int print_rev(va_list args);
+/**
+ * _printf - Custom printf function supporting %c, %s, and %% conversion specifiers.
+ *
+ * @format: A pointer to a string containing the format specifier and optional
+ *          text to be printed.
+ * @...: Additional arguments to be formatted and printed based on the format
+ *       specifier.
+ *
+ * Return: The total number of characters successfully printed to stdout.
+ *         If an error occurs, -1 is returned.
+ */
+int _printf(const char *format, ...)
+{
+    int count = 0;
+    va_list args;
 
-#endif /* MAIN_H */
+    if (format == NULL)
+        return -1;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; // Move past '%'
+            switch (*format)
+            {
+                case 'c':
+                    count += print_char(args);
+                    break;
+                case 's':
+                    count += print_str(args);
+                    break;
+                case '%':
+                    count += print_percent(args);
+                    break;
+                default:
+                    count += _putchar('%');
+                    count += _putchar(*format);
+            }
+        }
+        else
+        {
+            count += _putchar(*format);
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
 
