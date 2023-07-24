f9a65cf3a11cc21530e0448357eae1e27b4bcd0f
Collins Karweru <collinskarweru@gmail.com>
2023-07-24 13:00:05 -0700

    commit

--- main.h
+++ main.h
@@ -1,70 +1,26 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
-#include <stdlib.h>
-#include "main.h"
 
 /**
- * _putchar - Helper function to write a single character to stdout.
+ * struct specifier - Struct to store format specifiers and their corresponding
+ *                     printing functions.
  *
- * @c: The character to be written.
- * Return: On success, returns the number of characters written.
- * On error, returns -1 and sets errno appropriately.
+ * @spec: The format specifier character.
+ * @print: The function pointer to the corresponding printing function.
  */
-static int _putchar(char c)
+typedef struct specifier
 {
-    return write(1, &c, 1);
-}
+    char spec;
+    int (*print)(va_list);
+} set;
 
-/**
- * _printf - Custom printf function supporting %c, %s, and %% conversion specifiers.
- *
- * @format: A pointer to a string containing the format specifier and optional
- *          text to be printed.
- * @...: Additional arguments to be formatted and printed based on the format
- *       specifier.
- *
- * Return: The total number of characters successfully printed to stdout.
- *         If an error occurs, -1 is returned.
- */
-int _printf(const char *format, ...)
-{
-    int count = 0;
-    va_list args;
+/* Function prototypes */
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(va_list args);
 
-    if (format == NULL)
-        return -1;
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++; // Move past '%'
-            switch (*format)
-            {
-                case 'c':
-                    count += print_char(args);
-                    break;
-                case 's':
-                    count += print_str(args);
-                    break;
-                case '%':
-                    count += print_percent(args);
-                    break;
-                default:
-                    count += _putchar('%');
-                    count += _putchar(*format);
-            }
-        }
-        else
-        {
-            count += _putchar(*format);
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
-}
+#endif /* MAIN_H */
 
